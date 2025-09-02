505230d3b8fb297ee521ce0e880eab3b0abd1c39
Minsung <pauaup@naver.com>
2025-09-02 12:14:57 +0900

    initial upload

--- unittest/test_funcs.py
+++ unittest/test_funcs.py
@@ -1,6 +1,22 @@
-# TODO: 사용자 모듈 import
+from unit import is_odd, average, mymax, mymin
 
+def test_oddity():
+    assert is_odd(1) is True
+    assert is_odd(2) is False
+    assert is_odd(3) is True
+    assert is_odd(42) is False
 
-# TODO: 아래의 코드를 삭제하고 unittest를 작성하세요.
-def test_always():
-    assert False, "Remove me!"
+def test_average():
+    assert average([1,2,3]) == 2
+    assert average([100, 1000]) == 550
+    assert average([999, 999, 999]) == 999 
+
+def test_max():
+    assert mymax([1,100,5]) == 100
+    assert mymax([1,2,3,4,5,6,7,8,9,10]) == 10
+    assert mymax([-100, -10, 0]) == 0
+
+def test_min():
+    assert mymin([1,100,5]) == 1
+    assert mymin([1,2,3,4,5,6,7,8,9,10]) == 1
+    assert mymin([-100, -10, 0]) == -100
---
+++ unittest/unit.py
@@ -0,0 +1,11 @@
+def is_odd(value):
+    return True if value % 2 else False
+
+def average(list):
+    return sum(list) / len(list)
+
+def mymax(list):
+    return sorted(list)[-1]
+
+def mymin(list):
+    return sorted(list)[0]
